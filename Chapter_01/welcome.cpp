ada87772392ce9f955dff93fdb659a90d8e700ac
Nesie Starvil <nesiestarvil@gmail.com>
2026-09-09 22:29:57 +0300

    Refactor welcome.cpp for better output formatting

--- Chapter_01/welcome.cpp
+++ Chapter_01/welcome.cpp
@@ -1,9 +1,19 @@
 // This program outputs the message "Hello, World!" to the monitor
-import std; // gain access to the C++ standard library
-int main() // C++ programs start by executing the function main
-{
-    std::cout << "Welcome to C++, "; // output "Hello, Wor ld!"
-    std::cout << "Welcome to Object Oriented Programming, "; // output "Hello, Wor ld!"
-    std::cout << "and Programming is fun"; // output "Hello, Wor ld!"
+//import std; // gain access to the C++ standard library
+//int main() // C++ programs start by executing the function main
+//{
+    //std::cout << "Welcome to C++, "; // output "Hello, Wor ld!"
+  //  std::cout << "Welcome to Object Oriented Programming, "; // output "Hello, Wor ld!"
+//    std::cout << "and Programming is fun"; // output "Hello, Wor ld!"
+//    return 0;
+//}
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to OOP" << endl;
+    cout << "Programming is fun" << endl;
+
     return 0;
 }
