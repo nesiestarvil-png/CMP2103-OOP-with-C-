15b6e16ed4360336744a0ec24feea37401126122
Nesie Starvil <nesiestarvil@gmail.com>
2026-09-09 22:35:54 +0300

    Implement current calculation based on time input
    Current calculator

--- Chapter_03/current_calculator.cpp
+++ Chapter_03/current_calculator.cpp
@@ -5,3 +5,32 @@
 //        ⎩  0 V            otherwise
 
 // The program should prompt the user to enter time, t.
+#include <iostream>
+using namespace std;
+
+int main() {
+    double t;
+    double C = 200e-6;   // 200 microfarads
+    double current;
+
+    cout << "Enter time t (seconds): ";
+    cin >> t;
+
+    if (t > 0 && t < 1) {
+        current = C * 50;
+    }
+    else if (t > 1 && t < 3) {
+        current = C * (-50);
+    }
+    else if (t > 3 && t < 4) {
+        current = C * 50;
+    }
+    else {
+        current = 0;
+    }
+
+    cout << "Current through the capacitor = "
+         << current << " A" << endl;
+
+    return 0;
+}
